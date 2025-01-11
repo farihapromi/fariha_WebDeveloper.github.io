0fcaa857e1f6e3b34106ccfb2482fcf75ed722a7
Fariha Promi <farihapromi2611@gmail.com>
2025-01-11 22:25:15 +0600

    changed header toggole

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,36 @@
-
+import { useEffect } from 'react';
 
 
 const Header = () => {
+  useEffect(() => {
+    const links = document.querySelectorAll(".nav-links a");
+    const toggleMenuButton = document.getElementById("toggle-menu");
+    const menu = document.getElementById("skills_project");
+
+    
+    links.forEach((link) => {
+      link.addEventListener("click", (event) => {
+       
+        links.forEach((link) => link.classList.remove("active"));
+        
+        event.target.classList.add("active");
+      });
+    });
+
+   
+    const handleToggleMenu = () => {
+      menu.classList.toggle("show");
+    };
+
+    toggleMenuButton.addEventListener("click", handleToggleMenu);
+
+  
+    return () => {
+      links.forEach((link) => link.removeEventListener("click", () => {}));
+      toggleMenuButton.removeEventListener("click", handleToggleMenu);
+    };
+  }, []);
+
   return (
     <header>
       <h1 className="head-name">Fariha Promi</h1>
